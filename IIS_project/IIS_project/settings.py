ae719d9b4ffc6e9eb056ea27469c0109e20e1e26
JanKoci <hozakoci@gmail.com>
2018-10-26 23:46:01 +0200

    created first version of navbar

--- IIS_project/IIS_project/settings.py
+++ IIS_project/IIS_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'patients',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
