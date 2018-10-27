ee601631b7b5f98a6fea45534d8ef16ee19aca19
JanKoci <hozakoci@gmail.com>
2018-10-27 16:38:34 +0200

    started patients views

--- IIS_project/IIS_project/urls.py
+++ IIS_project/IIS_project/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from IIS_project import views
 
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
+    path('patients/', include('patients.urls', namespace='patients')),
+    path('admin/', admin.site.urls),
 ]
--- IIS_project/patients/views.py
+++ IIS_project/patients/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+# from django.http import HttpResponse
 
 # Create your views here.
+class Patient(TemplateView):
+    template_name = 'patients/patient_list.html'
