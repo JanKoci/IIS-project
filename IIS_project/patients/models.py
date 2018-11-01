5cde08840efd0b55d28028be867fbd4f0ba334d1
JanKoci <hozakoci@gmail.com>
2018-11-01 18:39:50 +0100

    started patients model

--- IIS_project/patients/models.py
+++ IIS_project/patients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# class Patient(models.Model):
+#     person_id = models.Varchar(10, primary_key=True)
+#     first_name = models.Varchar(25)
+#     last_name = models.Varchar(25)
+#     birth_date = models.DateField()
+#     address_city = models.Varchar(50)
+#     address_street = models.Varchar(50)
+#     address_psc = models.Varchar(5)
+#     address_country = models.Varchar(50)
+#     phone_number = models.DecimalField(13)
+#     insurance = models.Varchar(50)
